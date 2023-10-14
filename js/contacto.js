46c60e02f9baf1f4d91af8df1bc84d8f9438304e
AJRMRODMAR <antoniodejesusrodriguez928@gmail.com>
2023-10-13 19:03:14 -0600

    publicv2

--- js/contacto.js
+++ js/contacto.js
@@ -1,17 +1,17 @@
     document.getElementById("whatsapp-button").addEventListener("click", function() {
-            window.open("https://wa.me/+524612668518", "_blank");
+            window.open("https://wa.me/+524612668518", "_blank", "noopener,noreferrer");
         });
 
         document.getElementById("youtube-button").addEventListener("click", function() {
-            window.open("https://www.youtube.com/@soydalto", "_blank");
+            window.open("https://www.youtube.com/@soydalto", "_blank", "noopener,noreferrer");
         });
         document.getElementById("correo-button").addEventListener("click", function() {
-            window.open("mailto:imarketingandpublicity@outlook.com", "_blank");
+            window.open("mailto:imarketingandpublicity@outlook.com", "_blank", "noopener,noreferrer");
         });
 
         document.getElementById("instagram-button").addEventListener("click", function() {
-            window.open("https://instagram.com/ingeniosmex?igshid=MzRlODBiNWFlZA==", "_blank");
+            window.open("https://instagram.com/ingeniosmex?igshid=MzRlODBiNWFlZA==", "_blank", "noopener,noreferrer");
         });
         document.getElementById("facebook-button").addEventListener("click", function() {
-            window.open("https://www.facebook.com/profile.php?id=100088862331341&mibextid=V3Yony", "_blank");
+            window.open("https://www.facebook.com/profile.php?id=100088862331341&mibextid=V3Yony", "_blank", "noopener,noreferrer");
         });
