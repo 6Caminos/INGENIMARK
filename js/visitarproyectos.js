ef1d65aee06cc2631b29c731d91ca44c4c0b7d35
6Caminos <antoniodejesusrodriguez928@gmail.com>
2023-10-19 02:08:44 -0600

    segundo proyecto

--- js/visitarproyectos.js
+++ js/visitarproyectos.js
@@ -2,3 +2,7 @@
 document.getElementById("btn_ir_a_rodmar").addEventListener("click", function() {
     window.open("#", "_blank", "noopener,noreferrer");
 });
+
+document.getElementById("btn_ir_a_comedor").addEventListener("click", function() {
+    window.open("#", "_blank", "noopener,noreferrer");
+});
